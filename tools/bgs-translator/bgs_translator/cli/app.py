8e19df8c80b966156b3408b9a41ebc065ee6dbd9
BB84 <hustphysicscheng@gmail.com>
2026-06-07 18:08:57 -0400

    feat(translator-tool): Chunk J edit + validate CLI commands

--- tools/bgs-translator/bgs_translator/cli/app.py
+++ tools/bgs-translator/bgs_translator/cli/app.py
@@ -13,10 +13,12 @@ import typer
 from bgs_translator import __version__
 from bgs_translator.cli.batch import batch_app
 from bgs_translator.cli.config import config_app
+from bgs_translator.cli.edit import edit_app
 from bgs_translator.cli.envelopes import success
 from bgs_translator.cli.inspect import inspect_app
 from bgs_translator.cli.profile import profile_app
 from bgs_translator.cli.project import project_app
+from bgs_translator.cli.validate import validate_app
 
 app = typer.Typer(no_args_is_help=True)
 app.add_typer(batch_app, name="batch")
@@ -24,6 +26,8 @@ app.add_typer(config_app, name="config")
 app.add_typer(project_app, name="project")
 app.add_typer(inspect_app, name="inspect")
 app.add_typer(profile_app, name="profile")
+app.add_typer(edit_app, name="edit")
+app.add_typer(validate_app, name="validate")
 
 
 @app.callback()
